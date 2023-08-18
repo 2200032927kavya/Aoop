4895c01b604fce053838f3b79ea5a62d4ed7acf9
2200032927kavya <140156093+2200032927kavya@users.noreply.github.com>
2023-08-18 14:32:09 +0530

    program on 18/8/23
    third commit

---
+++ HashtableDemo.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.util.Hashtable;
+ 	 
+ 	class HashtableDemo
+	{
+	public static void main(String args[])
+	{
+	Hashtable<String,Long>hm=new Hashtable<String,Long>();
+	String name;
+	long phno;
+	Scanner sc=new Scanner(System.in);
+
+	while(true)
+	{
+	System.out.println("1.enter phone key value pairs");
+	System.out.println("2.retrive values from keys");
+	System.out.println("3.display all keys");
+	System.out.println("4.exit");
+	System.out.println("enter ur choice");
+	int choice=sc.nextInt();
+
+
+
+	switch(choice)
+	{
+	case 1:
+	System.out.println("enter name");
+	name=sc.next();
+	System.out.println("enter phno");
+	phno=sc.nextLong();
+	hm.put(name,phno);
+	break;
+
+
+	case 2:
+	System.out.println("enter name");
+	name=sc.next();
+	phno=hm.get(name);
+	System.out.println("phno="+phno);
+	break;
+
+
+	case 3:
+	Set<String>set=new HashSet<String>();
+	set=hm.keySet();
+	System.out.println(set);
+	break;
+	default:return;
+	}
+	}
+	}
+}
+
+
+
+
+	
